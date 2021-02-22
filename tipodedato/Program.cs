c4bdcd5e40236fe510a93b579be3ec092f3cde9d
Vanidad <57409926+vanesa33@users.noreply.github.com>
2021-02-22 16:22:11 -0300

    ingresar un dato
    Crear un programa que lea una letra tecleada por el usuario y diga si se trata de una
    vocal, una cifra numérica o una consonante (pista: habrá que usar un dato de tipo
    "char").

---
+++ tipodedato/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Ejercicio_Propuesto_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char TECLA;
+            string linea;
+            Console.Write("DIGITE UNA SOLA TECLA");
+            Console.WriteLine();
+            linea = Console.ReadLine();
+            TECLA = char.Parse(linea);
+            switch (TECLA)
+            {
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    ; Console.WriteLine("ES UNA CIFRA NUMERICA");
+                    break;
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    ; Console.WriteLine("ES UNA VOCAL");
+                    break;
+                default:
+                    Console.WriteLine("ES UNA CONSONANTE");
+                    break;
+            }
+            Console.Write("Pulse una Tecla:"); Console.ReadLine();
+        }
+    }
+}
